1fb367bfa8fb5720879d6f60534e615c00193ac3
dreamforxou <shsh9600@naver.com>
2024-06-23 05:38:37 +0900

    feat : 회원가입 api 연동, 로그인 api 연동실패(토큰......)

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,13 +1,25 @@
 export const inputFields = [
-    { id: 'name', type: 'text', placeholder: '이름', maxLength: 10 },
-    { id: 'userId', type: 'text', placeholder: '아이디', maxLength: 12 },
-    { id: 'password', type: 'password', placeholder: '비밀번호' }
-  ];
-  
-export  const teamOptions = ['비트버디', '아지토', '커플로그', '티그', '펫플레이트'];
+  { id: 'name', type: 'text', placeholder: '이름', maxLength: 10 },
+  { id: 'userId', type: 'text', placeholder: '아이디', maxLength: 12 },
+  { id: 'password', type: 'password', placeholder: '비밀번호' }
+];
+
+export interface TeamOption {
+  id: number;
+  name: string;
+}
+
+export const teamOptions: TeamOption[] = [
+  { id: 1, name: '비트버디' },
+  { id: 2, name: '아지토' },
+  { id: 3, name: '커플로그' },
+  { id: 4, name: '티그' },
+  { id: 5, name: '펫플레이트' },
+];
+
 export const departmentOptions = ['기획', '디자인', '프론트엔드', '백엔드'];
 
 export const loginInputFields = [
-    { id: 'userId', type: 'text', placeholder: '아이디' },
-    { id: 'password', type: 'password', placeholder: '비밀번호' }
-  ];
+  { id: 'userId', type: 'text', placeholder: '아이디' },
+  { id: 'password', type: 'password', placeholder: '비밀번호' }
+];
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,20 +1,36 @@
+export interface LoginFormData {
+  userId: string;
+  password: string;
+}
+
+export interface FormData {
+  name: string;
+  userId: string;
+  password: string;
+  confirmPassword: string;
+  email: string;
+  team: string;
+  department: string;
+}
+
 export interface DropdownProps {
-    label: string;
-    options: string[];
-    selectedOption: string;
-    setSelectedOption: (value: string) => void;
-    isOpen: boolean;
-    onOpen: () => void;
-    onClose: () => void;
-  }
-  
-  export interface FormData {
-    name: string;
-    username: string;
-    password: string;
-    confirmPassword: string;
-    email: string;
-    team: string;
-    department: string;
-  }
-  
+  label: string;
+  options: string[];
+  selectedOption: string;
+  setSelectedOption: (value: string) => void;
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+export interface InputFieldProps {
+  id: string;
+  type: string;
+  placeholder: string;
+  value: string;
+  touched: boolean;
+  error: string;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
+  handleClear: (id: string, value: string) => void;
+}
