f6f9a9fb091b6c5bab9c4029b478aae6a9196855
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-06-25 02:39:42 +0900

    feat: 프론트엔드 파트장 투표

--- src/components/PartLeaderVote/FrontEndVote.tsx
+++ src/components/PartLeaderVote/FrontEndVote.tsx
@@ -1,15 +1,55 @@
-import { FrontPartData } from '@/lib/data';
+'use client';
+import { candiateList, voteAction } from '@/lib/actions/voteAction';
+import { useRouter } from 'next/navigation';
+import { MouseEvent, useEffect, useState } from 'react';
+
+interface Candidate {
+  id: number;
+  name: string;
+  part: string;
+}
 
 export default function FrontEndVote() {
-  // const onClickVote = async (teamId: number) => {
-  //   const token = localStorage.getItem('token') || '';
-  //   try {
-  //     const response = await voteAction(teamId, token);
-  //     console.log(response);
-  //   } catch (error) {
-  //     console.error('투표 중 오류가 발생했습니다:', error);
-  //   }
-  // };
+  const [candidateList, setCandidateList] = useState<Candidate[]>([]);
+  const router = useRouter();
+
+  const onClickVote = async (teamId: number) => {
+    const token = localStorage.getItem('token') || '';
+    try {
+      const response = await voteAction(teamId, token);
+      console.log('투표 결과:', response);
+      if (response.ok) {
+        alert('투표가 완료되었습니다.');
+        router.push('/front-end-vote/result');
+      }
+    } catch (error) {
+      console.error('투표 중 오류가 발생했습니다:', error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchVoteResult = async () => {
+      const accessToken = localStorage.getItem('token') || '';
+      try {
+        const response = await candiateList(accessToken);
+
+        // Assuming the response has a `data` key that contains the array of candidates
+        const candidates = response.members || [];
+
+        if (Array.isArray(candidates)) {
+          // Select candidates with index 0 to 9
+          const filteredCandidates = candidates.slice(0, 10);
+          setCandidateList(filteredCandidates);
+          console.log('Candidates:', filteredCandidates);
+        } else {
+          console.error('Expected an array of candidates');
+        }
+      } catch (error) {
+        console.error('투표 결과 조회 중 오류 발생', error);
+      }
+    };
+    fetchVoteResult();
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center bg-BG-black text-white">
@@ -17,9 +57,12 @@ export default function FrontEndVote() {
         <h1 className="mb-8 text-center text-3xl font-bold">프론트 파트장 투표</h1>
         <div className="flex gap-12">
           <div className="flex flex-wrap justify-center gap-6 px-10">
-            {FrontPartData.map((data) => (
-              <div className="flex h-20 w-40 cursor-pointer flex-col items-center justify-center gap-1 rounded-lg border-2 border-main bg-BG-black text-center hover:bg-main active:bg-main">
-                <p className="text-xs text-gray-400">{data.team}</p>
+            {candidateList.map((data) => (
+              <div
+                key={data.id}
+                onClick={() => onClickVote(data.id)}
+                className="flex h-20 w-40 cursor-pointer flex-col items-center justify-center gap-1 rounded-lg border-2 border-main bg-BG-black text-center hover:bg-main active:bg-main">
+                <p className="text-xs text-gray-400">{data.part}</p>
                 <p className="text-xl font-semibold">{data.name}</p>
               </div>
             ))}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
         'BG-black': '#17181C',
-        'main': '#EE1171',
+        main: '#EE1171',
+        main2: '#FF4493',
+        sub2: '#480522',
+        sub1: '#8F0B48',
+        gray100: '#C3C5C9',
+        gray200: '#979A9F',
+        gray300: '#7C7F83',
+        gray400: '#4B4D4F',
+        gray500: '#313335',
+        gray700: '#28292A',
       },
     },
   },
