f58a0955bd1d783fe0a01d636489a9c24c105d96
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-06-25 02:39:42 +0900

    feat: 프론트엔드 파트장 투표

--- src/lib/actions/voteAction.ts
+++ src/lib/actions/voteAction.ts
@@ -3,7 +3,7 @@ export const voteAction = async (teamId: number, token: string) => {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
+      Access: `Bearer ${token}`,
     },
     body: JSON.stringify({
       teamId,
@@ -12,12 +12,24 @@ export const voteAction = async (teamId: number, token: string) => {
   return response;
 };
 
-export const voteResult = async () => {
+export const voteResult = async (token: string) => {
   const response = await fetch(`http://43.201.123.205:8080/votes/teams`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
+      Access: `Bearer ${token}`,
     },
   });
   return response;
+};
+
+export const candiateList = async (token: string) => {
+  const response = await fetch(`http://43.201.123.205:8080/votes/candidates`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Access: `Bearer ${token}`,
+    },
+  });
+  return response.json();
 };
